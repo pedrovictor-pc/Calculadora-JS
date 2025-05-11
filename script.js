024c2da237c4d54022c848c60b4a709bf63770bd
Pedro Victor <pedrovictorpereiracoelho@gmail.com>
2025-05-11 11:01:53 -0300

    Projeto de Calculadora 1.0
    Feito com JavaScript

---
+++ script.js
@@ -0,0 +1,87 @@
+let primeiroNumero = '';
+let segundoNumero = '';
+let operadorAtual = null;
+let resetarDisplay = false;
+
+const simbolosOperadores = {
+    '+': '+',
+    '-': '−',
+    '*': '×',
+    '/': '÷'
+};
+
+const display = document.getElementById('display');
+const botoes = document.querySelectorAll('button');
+
+for (const botao of botoes) {
+    botao.addEventListener('click', () => {
+        const valor = botao.getAttribute('data-valor');
+
+        if (botao.classList.contains('numero')) {
+            inserirNumero(valor);
+        } else if (botao.classList.contains('operador')) {
+            inserirOperador(valor);
+        } else if (botao.classList.contains('igual')) {
+            calcularResultado();
+        } else if (botao.classList.contains('limpar')) {
+            limparCalculadora();
+        }
+    })
+};
+
+function inserirNumero(num) {
+    if (resetarDisplay) {
+        display.value = '';
+        resetarDisplay = false
+    }
+    display.value = display.value + num;
+
+    if (operadorAtual === null) {
+        primeiroNumero = display.value
+    } else {
+        segundoNumero = display.value
+    }
+}
+
+function inserirOperador(op) {
+    if (primeiroNumero !== '' && operadorAtual !== null && segundoNumero !== '') {
+        calcularResultado()
+    }
+    operadorAtual = op;
+    primeiroNumero = display.value !== '' ? display.value : '0';
+    display.value = simbolosOperadores[op];
+    resetarDisplay = true
+}
+
+function calcularResultado() {
+    if (primeiroNumero === '' || operadorAtual === null) { return }
+    const num1 = Number(primeiroNumero);
+    const num2 = Number(segundoNumero || display.value);
+
+    let resultado;
+    if (operadorAtual === '+') {
+        resultado = num1 + num2;
+    } else if (operadorAtual === '-') {
+        resultado = num1 - num2;
+    } else if (operadorAtual === '*') {
+        resultado = num1 * num2;
+    } else if (operadorAtual === '/') {
+        resultado = num2 !== 0 ? num1 / num2 : 'Erro';
+    } else {
+        return;
+    }
+
+    display.value = resultado === 'Erro' ? 'Erro' : resultado;
+    primeiroNumero = display.value;
+    operadorAtual = null;
+    segundoNumero = '';
+    resetarDisplay = true;
+}
+
+function limparCalculadora() {
+    display.value = '';
+    primeiroNumero = '';
+    segundoNumero = '';
+    operadorAtual = null;
+    resetarDisplay = false
+}
